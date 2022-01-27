53c714b9726833626076f0a6398eb07c5bc4f317
staronearth <2249828201@qq.com>
2022-01-27 10:19:36 +0800

    go kafka connect

---
+++ go.mod
@@ -0,0 +1,14 @@
+module test-go-code
+
+go 1.17
+
+require (
+	github.com/segmentio/kafka-go v0.4.25
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
+)
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+)
---
+++ src/connectkafka/kafkaconn.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+func listtopic() {
+	conn, err := kafka.Dial("tcp", "172.17.32.1:30901")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	m := map[string]struct{}{}
+
+	for _, p := range partitions {
+		m[p.Topic] = struct{}{}
+	}
+	for k := range m {
+		fmt.Println(k)
+	}
+}
+func createtopic() {
+	// to create topics when auto.create.topics.enable='false'
+	topic := "my-topic"
+	partition := 3
+
+	conn, err := kafka.Dial("tcp", "172.17.32.1:30901")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+	var controllerConn *kafka.Conn
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer controllerConn.Close()
+
+	topicConfigs := []kafka.TopicConfig{
+		kafka.TopicConfig{
+			Topic:             topic,
+			NumPartitions:     partition,
+			ReplicationFactor: 1,
+		},
+	}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func producer() {
+	topic := "my-topic"
+	partition := 3
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "172.17.32.1:30901", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte("one!")},
+		kafka.Message{Value: []byte("two!")},
+		kafka.Message{Value: []byte("three!")},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
+
+func consumer() {
+	topic := "my-topic"
+	partition := 3
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "172.17.32.1:30901", topic, partition)
+	if err != nil {
+		log.Fatal("failed to dial leader:", err)
+	}
+
+	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	batch := conn.ReadBatch(10e3, 1e6) // fetch 10KB min, 1MB max
+
+	b := make([]byte, 10e3) // 10KB max per message
+	for {
+		n, err := batch.Read(b)
+		if err != nil {
+			break
+		}
+		fmt.Println(string(b[:n]))
+	}
+
+	if err := batch.Close(); err != nil {
+		log.Fatal("failed to close batch:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close connection:", err)
+	}
+}
+func main() {
+	//createtopic()
+	listtopic()
+	producer()
+	consumer()
+}
